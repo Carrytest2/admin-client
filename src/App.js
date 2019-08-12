d79fa22d00ebd634b8f96eb575a3f61108c66c06
tanlei <tanlei@163.com>
2019-08-12 15:41:17 +0800

    admin-client

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                哈哈哈45678911111111
+                哈哈哈456789111111112222222222
             </div>
         )
     }
